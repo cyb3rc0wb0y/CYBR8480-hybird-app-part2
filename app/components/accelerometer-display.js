078283aee131d81887a29a984264363ec06c651b
Jeff Smith <jeffreyasmith@unomaha.edu>
2019-09-24 00:26:06 -0500

    Added Chart

--- app/components/accelerometer-display.js
+++ app/components/accelerometer-display.js
@@ -1,18 +1,20 @@
 import Component from '@ember/component';
 import { later } from '@ember/runloop';
+import { A } from '@ember/array';
 
 export default Component.extend({
   x: 0,
   y: 0,
   z: 0,
   on: true,
-  init(){
+  accelHistory: A(),
+  init: function(){
     //begin logging accelerometer data once the component launches
     this._super(...arguments);
     this.updateAccelData(this)
 
   },
-  updateAccelData(component){
+  updateAccelData: function(component){
     later(function(){
       //wrapper to preserve binding satistfaction
       try {
@@ -22,6 +24,17 @@ export default Component.extend({
             component.set('x', acceleration.x);
             component.set('y', acceleration.y);
             component.set('z', acceleration.z);
+            var history=component.get('accelHistory');
+            if(history.length === 150){
+              history.shiftObject();//shift an x off
+              history.shiftObject();//shift a y off
+              history.shiftObject();//shift a z off
+            }
+            var t = Date.now();
+            var newXPoint = {time: t, label: 'x', value: acceleration.x};
+            var newYPoint = {time: t, label: 'y', value: acceleration.y};
+            var newZPoint = {time: t, label: 'z', value: acceleration.z};
+            history.addObjects([newXPoint, newYPoint, newZPoint]);
             console.log("accel vals: x: "+ acceleration.x+ " y: "+acceleration.y+" z: "+acceleration.z+" t: "+ Date.now());
         }, function (error) {//error callback
             console.log('error: ' + error);
